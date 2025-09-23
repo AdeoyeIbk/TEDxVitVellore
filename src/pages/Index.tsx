5214aab60f38a0032ccbae64ed867e6bccedcc59
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2025-09-23 18:23:25 +0100

    Implement code changes to enhance functionality and improve performance

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,6 @@
 import heroImg from "../assets/images/hero_img.png";
 import aboutImg from "../assets/images/about_img.png";
+import merakiImg from "../assets/images/meraki_img.png";
 import { motion, useInView } from "motion/react";
 import { useRef } from "react";
 
@@ -142,6 +143,87 @@ export default function Index() {
               transition: { duration: 0.3 },
             }}
           />
+        </motion.div>
+      </motion.section>
+
+      {/* upcoming events section */}
+      <motion.section
+        ref={aboutRef}
+        className="flex flex-col gap-32 justify-center items-center h-lvh p-24"
+        initial={{ opacity: 0 }}
+        animate={isAboutInView ? { opacity: 1 } : { opacity: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <motion.h2
+          className="text-3xl font-bold"
+          initial={{ opacity: 0, y: 50 }}
+          animate={isAboutInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+        >
+          Upcoming events
+        </motion.h2>
+        <motion.div
+          className="flex gap-32 justify-between"
+          initial={{ opacity: 0, y: 30 }}
+          animate={isAboutInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
+          transition={{ duration: 0.8, delay: 0.4 }}
+        >
+          <motion.img
+            src={merakiImg}
+            alt="speaker"
+            className="w-96 h-auto object-cover"
+            initial={{ opacity: 0, x: 50, scale: 0.8 }}
+            animate={
+              isAboutInView
+                ? { opacity: 1, x: 0, scale: 1 }
+                : { opacity: 0, x: 50, scale: 0.8 }
+            }
+            transition={{ duration: 0.8, delay: 0.7 }}
+            whileHover={{
+              scale: 1.05,
+              transition: { duration: 0.3 },
+            }}
+          />
+          <motion.div className="flex flex-col gap-2">
+            <motion.h3
+              className="text-2xl font-semibold mb-1"
+              initial={{ opacity: 0, y: 20 }}
+              animate={
+                isAboutInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }
+              }
+              transition={{ duration: 0.6, delay: 0.8 }}
+            >
+              Meraki
+            </motion.h3>
+            <motion.p className="font-light leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </motion.p>
+            <motion.div className="flex flex-col gap-2">
+              <motion.p className="font-bold leading-none">
+                30-31st Jan 2021
+              </motion.p>
+              <motion.p className="font-bold leading-none">
+                Anna Auditorium
+              </motion.p>
+            </motion.div>
+            <motion.div className="mt-4">
+              <motion.button
+                className=" rounded-full border-2 border-red-500 py-2 px-8 text-sm text-red-500 hover:cursor-pointer"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.4, delay: 0.8 }}
+                whileHover={{
+                  scale: 1.05,
+                  transition: { duration: 0.2 },
+                }}
+                whileTap={{ scale: 0.95 }}
+              >
+                Buy Tickets
+              </motion.button>
+            </motion.div>
+          </motion.div>
         </motion.div>
       </motion.section>
     </>
