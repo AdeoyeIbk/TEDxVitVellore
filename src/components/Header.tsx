e50bb681d14d164d7d2895e4f8905a55bdbab1e8
Adeoye Ibukunoluwa <adeoyeibukunoluwa3@gmail.com>
2025-09-23 18:55:19 +0100

    fix(Header): update header positioning and improve mobile menu styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,13 +14,13 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
+    <header className="fixed inset-x-0 top-0 z-50">
       <nav
         aria-label="Global"
         className="flex items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <img src={tedxLogo} alt="TEDx Logo" className="h-12 w-auto" />
           </a>
         </div>
@@ -52,15 +52,11 @@ export default function Header() {
         className="lg:hidden"
       >
         <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-black p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
+              <img alt="TEDx Logo" src={tedxLogo} className="h-8 w-auto" />
             </a>
             <button
               type="button"
